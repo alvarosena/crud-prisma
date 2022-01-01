b152c65a34e9f2c89e1456dc8059e30228d3877a
Alvaro Sena <alvarosenacs.c@gmail.com>
2022-01-01 16:36:49 -0300

    Validating if the user provided username and email to create an account & [fix: updating username without authentication.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata"
+import cors from 'cors';
 import express from 'express';
 import { routes } from './routes/routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -19,6 +19,14 @@ class CreateUserService {
 
   async execute({ username, email, password }: IRequest) {
 
+    if (!username || !email) {
+      throw new Error('Username or email was provided.');
+    }
+
+    if (!password) {
+      throw new Error('Password is incorrect');
+    }
+
     const hashPassword = await hash(password, 10);
 
     const user = await this.usersRepository.create(username, email, hashPassword);
